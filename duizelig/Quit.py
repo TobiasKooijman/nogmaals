cbfcaf3f649b53a6ee65c19af03eae610db3c115
Tobias Kooijman <99068704@mydavinci.nl>
2021-09-16 10:54:59 +0200

    al die opdr toegevoegd(WEET NIET OF TELLEN50 GOED IS)

---
+++ duizelig/Quit.py
@@ -0,0 +1,21 @@
+import time
+
+vraag = input("DON'T TYPE IN QUIT!!  ")
+
+while vraag != "quit":
+    time.sleep(1)
+    print("Okay, good.")
+    time.sleep(1)
+    print("you didn't type in quit")
+    time.sleep(1)
+    print("good job buddy!")
+    time.sleep
+    print("I'm proud of you")
+    vraag = input("DON'T TYPE IN QUIT!!  ")
+
+
+
+print("Bruh")
+time.sleep(1)
+print("I told you")
+exit()
---
+++ duizelig/tellen50.py
@@ -0,0 +1,18 @@
+import time
+i = 1
+g = 1
+b=50
+l=50
+t= 50
+for i in range(50,1000):
+    while l >= 50:
+        t = b + g
+        g = t + t
+        print(t)
+        print(g)
+        time.sleep(1)
+        while g >= 1000:
+            exit()
+
+##### IK WEET NIET OF IK DIT GOED HEB GEDAAN MAAR IK WAS HIER 2 UUR MEE BEZIG EN IK SNAP ER NOG STEEDS HELEMAAL NIKS VAN
+#FUCK DEZE OPDRACHT
