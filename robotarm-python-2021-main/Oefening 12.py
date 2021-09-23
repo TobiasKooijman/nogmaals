f3f1afbb43e3fc442abec62aefafc028d116cbb8
Tobias Kooijman <99068704@mydavinci.nl>
2021-09-23 12:11:31 +0200

    opdr 12 gemaakt en toegevoegd

--- robotarm-python-2021-main/Oefening 12.py
+++ robotarm-python-2021-main/Oefening 12.py
@@ -1,13 +1,16 @@
 ## Oefening 12
 from RobotArm import RobotArm
-
 robotArm = RobotArm('exercise 12')
-
 # Jouw python instructies zet je vanaf hier:
-
-## Nou, Nu kan je hem zelf besturen dus je kan het lekker zelf doen!
-
-robotArm.operate()
-
+for n in range(9):
+    robotArm.moveRight()
+for n in range(15):
+    robotArm.moveLeft()
+    robotArm.grab()
+    color = robotArm.scan()
+    if color == "red":
+        for n in range(9):
+            robotArm.moveRight()
+    robotArm.drop()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
